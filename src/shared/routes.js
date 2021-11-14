704cca68508141184ba7344a970420b2d260dfc3
Theryston Santos <funktodo2@gmail.com>
2021-11-14 15:58:10 -0300

    login

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -5,4 +5,13 @@ const movieRoutes = require('../modules/shows/http/routes')
 
 router.use('/show', movieRoutes)
 
+router.get('/login', (req, res) => {
+    const { username, password } = req.query;
+    if (username === 'redetv-admin' && password === 'J2*rHNFKijHtX4DAzIVcQuE6qAcY2V') {
+        res.json({ success: true })
+    } else {
+        res.status(401).json({ message: 'Invalid username and password' })
+    }
+})
+
 module.exports = router;
