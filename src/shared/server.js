bde572eb13e8f8b3c02ba7ea9f1ae2a871bae788
Theryston Santos <funktodo2@gmail.com>
2021-11-24 18:51:33 -0300

    ajustrs

--- src/shared/server.js
+++ src/shared/server.js
@@ -1,14 +1,6 @@
 const { app } = require('./app');
-const https = require('https');
-const path = require('path');
-const fs = require('fs');
 
 require('../modules/shows/robots/onedrive');
 require('../modules/shows/robots/setoffline');
 
-const sslServer = https.createServer({
-    key: fs.readFileSync(path.join(__dirname, '..', '..', 'cert', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, '..', '..', 'cert', 'cert.pem')),
-}, app)
-
-sslServer.listen(process.env.PORT, () => console.log('listening on port ' + process.env.PORT))
+app.listen(process.env.PORT, () => console.log('listening on port ' + process.env.PORT))
