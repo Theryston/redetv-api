0ba5e7298d2077b67c529056ad8e7cd2886fa2b2
Theryston Santos <funktodo2@gmail.com>
2021-11-24 15:37:17 -0300

    ajustr

--- src/config/multer.js
+++ src/config/multer.js
@@ -4,9 +4,13 @@ const crypto = require('crypto');
 
 module.exports = {
     dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-    storage: multer.memoryStorage(),
+    storage: multer.memoryStorage({
+        destination: function(req, file, callback) {
+            callback(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'));
+        },
+    }),
     limits: {
-        fileSize: 1024 * 1024 * 1024 * 1024, // 1 TB
+        fileSize: 1024 * 1024 * 1024 * 1024 * 1024, // 1 PB
     },
     fileFilter: (req, file, cb) => {
         const allowedMimes = [
